3939497d0d2b400edeaacfbc6091d7ef6e373cf6
Luis Fernando Jimenez Lopez <luisfernando.jimenez.lopez.contractor@bbva.com>
2020-08-15 02:20:43 -0500

    feat(*): basic file and folder structure

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Aquí va el código</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
